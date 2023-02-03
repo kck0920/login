eaffa2694e2829c9cda8598bbb4ec51ce7bbb7d6
해퓨쳐 <kck0920@gmail.com>
2023-02-03 16:30:27 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,15 +1,22 @@
-'use strict'
+'use strict';
 
-const id = document.querySelector("#id"),
-  password = document.querySelector("#password"),
-  loginBtn = document.querySelector("#btn_login");
+const id = document.querySelector('#id'),
+	password = document.querySelector('#password'),
+	loginBtn = document.querySelector('#btn_login');
 
-loginBtn.addEventListener("click", login);
+loginBtn.addEventListener('click', login);
 
 function login() {
-  const req = {
-    id: id.value,
-    password: password.value,
-  };
-  console.log(req)
+	const req = {
+		id: id.value,
+		password: password.value,
+	};
+
+	fetch('/login', {
+		method: 'POST',
+		header: {
+			'Content-type': 'application/json',
+		},
+		body: JSON.stringify(req),
+	});
 }
