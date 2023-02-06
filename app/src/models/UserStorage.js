304ffa2d81be689fc3dbc29b7d62495ef7988913
해퓨쳐 <kck0920@gmail.com>
2023-02-06 16:05:36 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,7 +2,7 @@
 
 class UserStorage {
   static #users = {
-	  id: ['malthael', '나개발', '김팀장'],
+	  id: ['hafuture', '나개발', '김팀장'],
 	  password: ['1234', '1234', '123456'],
     name: ['해퓨쳐', '나개발', '김팅장장'],
   };
@@ -17,6 +17,17 @@ class UserStorage {
 		}, {});
     console.log(newUsers)
 		return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users); // => [id, password, name]
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
   }
 }
 
