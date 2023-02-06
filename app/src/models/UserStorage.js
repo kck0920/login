808286cab73848729f30e947537c2b79517905c9
해퓨쳐 <kck0920@gmail.com>
2023-02-06 17:58:34 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,34 +1,42 @@
 'use strict';
 
 class UserStorage {
-  static #users = {
-	  id: ['hafuture', '나개발', '김팀장'],
-	  password: ['1234', '1234', '123456'],
-    name: ['해퓨쳐', '나개발', '김팅장장'],
-  };
+	static #users = {
+		id: ['hafuture', '나개발', '김팀장'],
+		password: ['1234', '1234', '123456'],
+		name: ['해퓨쳐', '나개발', '김팅장장'],
+	};
 
-  static getUsers(...fields) {
-    const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
+	static getUsers(...fields) {
+		const users = this.#users;
+		const newUsers = fields.reduce((newUsers, field) => {
 			if (users.hasOwnProperty(field)) {
 				newUsers[field] = users[field];
 			}
 			return newUsers;
 		}, {});
-    console.log(newUsers)
+		console.log(newUsers);
 		return newUsers;
-  }
+	}
 
-  static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const userKeys = Object.keys(users); // => [id, password, name]
-    const userInfo = userKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
-  }
+	static getUserInfo(id) {
+		const users = this.#users;
+		const idx = users.id.indexOf(id);
+		const userKeys = Object.keys(users); // => [id, password, name]
+		const userInfo = userKeys.reduce((newUser, info) => {
+			newUser[info] = users[info][idx];
+			return newUser;
+		}, {});
+		return userInfo;
+	}
+
+	static save(userInfo) {
+		const users = this.#users;
+		users.id.push(userInfo.id);
+		users.name.push(userInfo.name);
+		users.password.push(userInfo.password);
+		return { success: true };
+	}
 }
 
 module.exports = UserStorage;
