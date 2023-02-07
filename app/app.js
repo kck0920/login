420a0bd0ebee34ac908ba41e6623dd0586e524b9
해퓨쳐 <kck0920@gmail.com>
2023-02-07 19:22:26 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,21 +1,6 @@
-// const http = require('http');
-// const app = http.createServer((req, res) => {
-// 	res.writeHead(200, {
-// 		'content-type': 'text/html; charset=utf-8'
-// 	});
-// 	if (req.url === '/') {
-// 		res.end(`여기는 루트페이지 입니다.`);
-// 	} else if (req.url === '/login') {
-// 		res.end(`여기는 로그인페이지 입니다.`);
-// 	}
-// });
-
-// app.listen(3000, () => {
-// 	console.log(`http로 가동된 서버입니다.`);
-// });
+'use strict';
 
 // 모듈
-'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
